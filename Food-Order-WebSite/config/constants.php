dbd93a6bbfcaad04620e4ef5582cfff2c50da5cb
Zakka24 <aoukaili.zakaria@gmail.com>
2024-11-11 20:21:29 +0100

    added constant php file to make the code more modular

---
+++ Food-Order-WebSite/config/constants.php
@@ -0,0 +1,23 @@
+<?php
+    // Start a session
+    session_start();
+
+
+    define('SITEURL', 'http://localhost/Food-Order-WebSite/');
+    
+    // Costants to connect to the DB
+    define('LOCALHOST', 'localhost');
+    define('DB_USER', 'root');
+    define('PASSWORD', '');
+    define('DB_NAME', 'food-order');
+
+    // Connection to MySQL and database
+    $conn = mysqli_connect(LOCALHOST, DB_USER, PASSWORD);
+    if(!$conn){
+        die("Errore di connessione a MySQL: ". mysqli_connect_error());
+    }
+    $db_select = mysqli_select_db($conn, DB_NAME);
+    if(!$db_select){
+        die("Errore di connessione al database food-order: " . mysqli_error($conn));
+    }
+?>
