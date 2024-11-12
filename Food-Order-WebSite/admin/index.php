212aee7b39a6ab41b4a4210851e7409c4f51878f
Zakka24 <aoukaili.zakaria@gmail.com>
2024-11-12 23:27:53 +0100

    added display of succesful login

--- Food-Order-WebSite/admin/index.php
+++ Food-Order-WebSite/admin/index.php
@@ -4,6 +4,15 @@
             <div class="wrapper">
                 <h1>DASHBOARD</h1>
 
+                <br><br><br>
+                <?php 
+                    // Message for successfull login
+                    if(isset($_SESSION['user-logged'])){
+                        echo $_SESSION['user-logged']; // Display session message
+                        unset($_SESSION['user-logged']); // Remove session message
+                    }
+                ?>
+
                 <div class="col-4 text-center">
                     <h1>5</h1>
                     <br>
