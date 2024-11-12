6a555461f9d37aa24fc22f6446c37c8e3889d870
Zakka24 <aoukaili.zakaria@gmail.com>
2024-11-12 23:28:45 +0100

    added login-check code and include() statement in menu.php

--- Food-Order-WebSite/admin/partials/menu.php
+++ Food-Order-WebSite/admin/partials/menu.php
@@ -1,4 +1,7 @@
-<?php include('../config/constants.php');?>
+<?php 
+    include('../config/constants.php');
+    include('login-check.php');
+?>
 
 <html>
     <head>
@@ -8,7 +11,7 @@
 
 
     <body>
-        <!-- Menu Section Starts -->
+
          <div class="menu text-center">
             <div class="wrapper">
                 <ul>
@@ -17,8 +20,11 @@
                     <li><a href="manage-category.php">Category</a></li>
                     <li><a href="manage-food.php">Food</a></li>
                     <li><a href="manage-order.php">Order</a></li>
+                    <li><a href="logout.php">Logout</a></li>
                 </ul>
             </div>
          </div>
-        <!-- Menu Section Ends -->
+
+          
+
          
