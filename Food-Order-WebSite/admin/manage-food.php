12eaf7cc09eb49458344c03864864c6bf5924e7e
Zakka24 <aoukaili.zakaria@gmail.com>
2024-11-16 17:04:09 +0100

    added delete/update features. Display correct foods

--- Food-Order-WebSite/admin/manage-food.php
+++ Food-Order-WebSite/admin/manage-food.php
@@ -2,49 +2,101 @@
 
         <div class="main-content">
             <div class="wrapper">
-                <h1>Manage Food</h1>
+                <h1>Manage Category</h1>
 
-                <br> <br>
-                <a href="#" class="btn-primary">Add Food</a>
+                <br>
+
+                <?php 
+                    // Message for add category
+                    if(isset($_SESSION['add-food'])){
+                        echo $_SESSION['add-food']; // Display session message
+                        unset($_SESSION['add-food']); // Remove session message
+                    }
+                    
+                    // Message for delete category
+                    if(isset($_SESSION['delete-food'])){
+                        echo $_SESSION['delete-food']; // Display session message
+                        unset($_SESSION['delete-food']); // Remove session message
+                    }
+                    
+                    // Message for update category
+                    if(isset($_SESSION['update-food'])){
+                        echo $_SESSION['update-food']; // Display session message
+                        unset($_SESSION['update-food']); // Remove session message
+                    }
+                ?>
+
+                <br> <br> <br>
+                <a href="add-food.php" class="btn-primary">Add Food</a>
                 <br> <br>
 
                 <table class="tbl-full">
                     <tr>
                         <th>S.N.</th>
-                        <th>Full Name</th>
-                        <th>Username</th>
+                        <th>Title</th>
+                        <th>Desciption</th>
+                        <th>Price</th>
+                        <th>Image</th>
+                        <th>Category ID</th>
+                        <th>Featured</th>
+                        <th>Active</th>
                         <th>Actions</th>
                     </tr>
 
-                    <tr>
-                        <td>1.</td>
-                        <td>Zakaria Aoukaili</td>
-                        <td>Zakka</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Admin</a>
-                            <a href="#" class="btn-danger">Delete Admin</a>
-                        </td>
-                    </tr>
+                    <?php
+                    // Query to get all categories
+                    $sql = "SELECT * FROM tal_food";
+                    
+                    // Execute the query
+                    $res = mysqli_query($conn, $sql);
 
-                    <tr>
-                        <td>2.</td>
-                        <td>Zakaria Aoukaili</td>
-                        <td>Zakka</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Admin</a>
-                            <a href="#" class="btn-danger">Delete Admin</a>
-                        </td>
-                    </tr>
+                    // Check if the query is executed
+                    if($res){
+                        // Count rows to check if we have data
+                        $rows = mysqli_num_rows($res);
+                        if($rows > 0){
+                            while($rows = mysqli_fetch_assoc($res)){
+                                $id = $rows['id'];
+                                $title = $rows['title'];
+                                $description = $rows['description'];
+                                $price = $rows['price'];
+                                $image_name = $rows['image_name'];
+                                $category = $rows['category_id'];
+                                $featured = $rows['featured'];
+                                $active = $rows['active'];
+                                ?>
+                                <tr>
+                                    <td><?php echo $id; ?></td>
+                                    <td><?php echo $title; ?></td>
+                                    <td><?php echo $description; ?></td>
+                                    <td><?php echo $price; ?></td>
+                                    <td>
+                                        <?php 
+                                            if(!empty($image_name)){
+                                        ?>
+                                                <img src="<?php echo SITEURL;?>images/food/<?php echo $image_name;?>" width="100p">
+                                        <?php
+                                            }
+                                            else{
+                                                echo "<div class='error'>Image not Added</div>";
+                                            }
+                                        ?>
+                                    </td>
+                                    <td><?php echo $category; ?></td>
+                                    <td><?php echo $featured; ?></td>
+                                    <td><?php echo $active; ?></td>
+                                    <td>
+                                        <a href="update-food.php?id=<?php echo $id;?>" class="btn-secondary">Update Food</a> <br><br><!-- Passo l'id come query nell'url --->
+                                        <a href="delete-food.php?id=<?php echo $id;?>" class="btn-danger">Delete Food</a> <!-- Passo l'id come query nell'url --->
+                                    </td>
+                                </tr>
 
-                    <tr>
-                        <td>3.</td>
-                        <td>Zakaria Aoukaili</td>
-                        <td>Zakka</td>
-                        <td>
-                            <a href="#" class="btn-secondary">Update Admin</a>
-                            <a href="#" class="btn-danger">Delete Admin</a>
-                        </td>
-                    </tr>
+
+                                <?php
+                            }
+                        }
+                    }
+                ?>
                 </table>
             </div>
          </div>
